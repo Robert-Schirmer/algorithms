ff2dcc5c383e8957bb885923cc29a7bc32150c83
Robert Schirmer <rhsblue@gmail.com>
2022-05-30 16:20:34 -0400

    letterCombinations

---
+++ src/letterCombinations/letterCombinations.js
@@ -0,0 +1,58 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+const letterCombinations = (digits) => {
+  if (!digits) return [];
+
+  const phoneNumberLetterMap = {
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "x", "y", "z"],
+  };
+
+  const digitsArray = digits.split("");
+  let numOfcombinations = 1;
+  const digitLetterOptionsIndex = digitsArray.map((digit) => {
+    numOfcombinations *= phoneNumberLetterMap[digit].length;
+    return { currentLetterIndex: 0, maxIndex: phoneNumberLetterMap[digit].length - 1 };
+  });
+  const combinations = new Array(numOfcombinations);
+
+  for (let combinationsIndex = 0; combinationsIndex < combinations.length; combinationsIndex++) {
+    combinations[combinationsIndex] = digitsArray
+      .map((digit, digitIndex) => {
+        const letterOptions = phoneNumberLetterMap[digit];
+        const currentLetterIndex = digitLetterOptionsIndex[digitIndex].currentLetterIndex;
+        const returnLetter = letterOptions[currentLetterIndex];
+
+        if (combinationsIndex !== combinations.length - 1 && digitIndex === digitsArray.length - 1) {
+          const bumpDigitLetterIndex = (digitIndex) => {
+            if (digitLetterOptionsIndex[digitIndex].currentLetterIndex === digitLetterOptionsIndex[digitIndex].maxIndex) {
+              // Reset to 0
+              digitLetterOptionsIndex[digitIndex].currentLetterIndex = 0;
+              // Bump the higher up digitIndex
+              bumpDigitLetterIndex(digitIndex - 1);
+            } else {
+              digitLetterOptionsIndex[digitIndex].currentLetterIndex += 1;
+            }
+          };
+
+          bumpDigitLetterIndex(digitIndex);
+        }
+
+        return returnLetter;
+      })
+      .join("");
+  }
+
+  return combinations;
+};
+
+export default letterCombinations;
