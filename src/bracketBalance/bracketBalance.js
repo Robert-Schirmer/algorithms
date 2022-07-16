355c671488980d3fdf3454620db44508fbf28123
Robert Schirmer <rhsblue@gmail.com>
2022-07-16 15:09:58 -0400

    bracketBalance: unit tests setup

---
+++ src/bracketBalance/bracketBalance.js
@@ -0,0 +1,15 @@
+/**
+ * Given a string expression, write a program (balanced() method) to examine whether the pairs and the orders of
+ * “{“, “}”, “(“, “)”, “[“, “]” are correct in exp (all other chars are ignored). If they are correct, it is balanced.
+ * If balanced, just return "Balanced".
+ * If not balanced, return "Not Balanced: <problem-index>", where problem-index is the index of the first bracket
+ * that caused un-balancing.
+ * @param {string} input
+ * @return {string} Balance status
+ */
+const bracketBalance = (input) => {
+
+  return "Balanced";
+};
+
+export default bracketBalance;
